1b9b17f69170573a0c9117af35a071ccb55aa207
WojciechKoz <39268506+WojciechKoz@users.noreply.github.com>
2022-11-04 12:40:37 +0100

    feat: added omegaConf support in LOL datamodule

--- src/datamodules/lol.py
+++ src/datamodules/lol.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Callable, Optional, Union
+from typing import Callable, Dict, Optional
 
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, random_split
@@ -10,18 +10,18 @@ from src.datasets.lol import LOL  # noqa: I900
 class LOLDataModule(pl.LightningDataModule):
     def __init__(
         self,
-        data_path: Union[Path, str] = "data/LOL-fair",
-        batch_size: int = 32,
+        config: Dict,
         train_transform: Optional[Callable] = None,
         test_transform: Optional[Callable] = None,
-        val_size: float = 0.05,
-        preload_dataset: bool = False,
     ):
         super().__init__()
-        self.data_path = Path(data_path) if isinstance(data_path, str) else data_path
-        self.batch_size = batch_size
-        self.val_size = val_size
-        self.preload_dataset = preload_dataset
+        self.data_path = Path(config["path"])
+        self.batch_size = config["batch_size"]
+        self.val_size = config["val_size"]
+        self.preload_dataset = config["preload"]
+
+        self.pin_memory = config["pin_memory"]
+        self.num_workers = config["num_workers"]
 
         self.train_transform = train_transform
         self.test_transform = test_transform
@@ -46,29 +46,30 @@ class LOLDataModule(pl.LightningDataModule):
             pair_transform=self.test_transform,
             preload=self.preload_dataset,
         )
-        self.LOL_predict = LOL(
-            self.data_path,
-            train=False,
-            pair_transform=self.test_transform,
-            preload=self.preload_dataset,
-        )
 
     def train_dataloader(self):
-        return DataLoader(self.LOL_train, batch_size=self.batch_size)
+        return DataLoader(
+            self.LOL_train,
+            batch_size=self.batch_size,
+            pin_memory=self.pin_memory,
+            num_workers=self.num_workers,
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.LOL_val, batch_size=self.batch_size)
+        return DataLoader(
+            self.LOL_val,
+            batch_size=self.batch_size,
+            pin_memory=self.pin_memory,
+            num_workers=self.num_workers,
+        )
 
     def test_dataloader(self):
-        return DataLoader(self.LOL_test, batch_size=self.batch_size)
+        return DataLoader(
+            self.LOL_test,
+            batch_size=self.batch_size,
+            pin_memory=self.pin_memory,
+            num_workers=self.num_workers,
+        )
 
     def predict_dataloader(self):
-        return DataLoader(self.LOL_predict, batch_size=self.batch_size)
-
-
-if __name__ == "__main__":
-    lol_dm = LOLDataModule()
-    lol_dm.setup()
-    first_batch = next(iter(lol_dm.train_dataloader()))
-    print(first_batch["image"].size())
-    print(first_batch["target"].size())
+        return self.test_transform()
