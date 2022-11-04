7843039f4b6644253788115a62c0245e1814ea7f
WojciechKoz <39268506+WojciechKoz@users.noreply.github.com>
2022-11-04 11:24:44 +0100

    feat: support for paired transforms and images preloading in LOL dataset

--- src/datamodules/lol.py
+++ src/datamodules/lol.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Optional, Union
+from typing import Callable, Optional, Union
 
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, random_split
@@ -9,21 +9,49 @@ from src.datasets.lol import LOL  # noqa: I900
 
 class LOLDataModule(pl.LightningDataModule):
     def __init__(
-        self, data_path: Union[Path, str] = "data/LOL-fair", batch_size: int = 32
+        self,
+        data_path: Union[Path, str] = "data/LOL-fair",
+        batch_size: int = 32,
+        train_transform: Optional[Callable] = None,
+        test_transform: Optional[Callable] = None,
+        val_size: float = 0.05,
+        preload_dataset: bool = False,
     ):
         super().__init__()
         self.data_path = Path(data_path) if isinstance(data_path, str) else data_path
         self.batch_size = batch_size
+        self.val_size = val_size
+        self.preload_dataset = preload_dataset
+
+        self.train_transform = train_transform
+        self.test_transform = test_transform
 
     def prepare_data(self) -> None:
         pass
 
     def setup(self, stage: Optional[str] = None):
-        LOL_full = LOL(self.data_path, train=True)
+        LOL_full = LOL(
+            self.data_path,
+            train=True,
+            pair_transform=self.train_transform,
+            preload=self.preload_dataset,
+        )
 
-        self.LOL_train, self.LOL_val = random_split(LOL_full, [0.95, 0.05])
-        self.LOL_test = LOL(self.data_path, train=False)
-        self.LOL_predict = LOL(self.data_path, train=False)
+        self.LOL_train, self.LOL_val = random_split(
+            LOL_full, [1 - self.val_size, self.val_size]
+        )
+        self.LOL_test = LOL(
+            self.data_path,
+            train=False,
+            pair_transform=self.test_transform,
+            preload=self.preload_dataset,
+        )
+        self.LOL_predict = LOL(
+            self.data_path,
+            train=False,
+            pair_transform=self.test_transform,
+            preload=self.preload_dataset,
+        )
 
     def train_dataloader(self):
         return DataLoader(self.LOL_train, batch_size=self.batch_size)
--- src/datasets/lol.py
+++ src/datasets/lol.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Callable, Optional
 
 from torch.utils.data import Dataset
 from torchvision.io import read_image
@@ -7,23 +8,62 @@ from src.datasets.meta import PairedImageInput  # noqa: I900
 
 
 class LOL(Dataset):
-    def __init__(self, root: Path, train: bool = True):
+    def __init__(
+        self,
+        root: Path,
+        train: bool = True,
+        pair_transform: Optional[Callable] = None,
+        preload: bool = False,
+    ):
+        self.pair_transform = pair_transform
+
         path = root / ("our485" if train else "eval15")
 
-        self.images = sorted((path / "low/").glob("*.png"), key=lambda x: int(x.stem))
-        self.targets = sorted((path / "high/").glob("*.png"), key=lambda x: int(x.stem))
+        self.image_names = sorted(
+            (path / "low/").glob("*.png"), key=lambda x: int(x.stem)
+        )
+        self.target_names = sorted(
+            (path / "high/").glob("*.png"), key=lambda x: int(x.stem)
+        )
+
+        self.loaded = preload
+        if self.loaded:
+            self.load_all_images()
+
+    def load_all_images(self) -> None:
+        self.loaded_images_ = []
+        self.loaded_targets_ = []
+
+        for index in range(len(self)):
+            self.loaded_images_.append(read_image(str(self.image_names[index])))
+            self.loaded_targets_.append(read_image(str(self.image_names[index])))
 
     def __len__(self) -> int:
-        return len(self.images)
+        return len(self.image_names)
 
     def __getitem__(self, index: int) -> PairedImageInput:
-        image = read_image(str(self.images[index]))
-        target = read_image(str(self.targets[index]))
+        image = (
+            self.loaded_images_[index]
+            if self.loaded
+            else read_image(str(self.image_names[index]))
+        )
+        target = (
+            self.loaded_targets_[index]
+            if self.loaded
+            else read_image(str(self.target_names[index]))
+        )
+
+        if self.pair_transform:
+            image, target = self.pair_transform(image, target)
 
         return PairedImageInput(image=image, target=target)
 
 
 if __name__ == "__main__":
-    ds = LOL(Path("data/LOL-fair"), train=True)
-    print(ds[0])
-    print(len(ds))
+    from timeit import default_timer as timer
+
+    ds = LOL(Path("data/LOL-fair"), train=True, preload=False)
+    start = timer()
+    print(f'Image size: {ds[0]["image"].size()}')
+    stop = timer()
+    print(f"get item time: {stop - start:.5f} sec")
