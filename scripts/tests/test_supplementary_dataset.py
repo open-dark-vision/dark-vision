147cb8007f1e46a8c937b6dfab1d90d3729ca631
Anna Klimiuk <ania.klimiuk411@gmail.com>
2022-11-06 12:02:32 +0100

    feat: add supplementary evaluation datasets

---
+++ scripts/tests/test_supplementary_dataset.py
@@ -0,0 +1,42 @@
+"""
+Run it to make sure that your Supplemntary dataset configs are ok.
+"""
+import argparse
+from timeit import default_timer as timer
+
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+from omegaconf import OmegaConf
+
+from src.datamodules import SupplementaryDataModule  # noqa: I900
+from src.datasets.supplementary import SupplementaryNames  # noqa: I900
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config", type=str, default="configs/tests/supplementary_dataset_test.yaml"
+    )
+    args = parser.parse_args()
+    conf = OmegaConf.load(args.config)
+    print("Configs:\n", OmegaConf.to_yaml(conf), "", sep="*" * 50 + "\n")
+
+    transform = A.Compose([A.Resize(512, 512), ToTensorV2()])
+
+    for dataset in SupplementaryNames:
+        print(f"Testing {dataset.name} dataset")
+        conf["dataset"]["name"] = dataset.value
+
+        setup_timer_start = timer()
+        supplementary_dm = SupplementaryDataModule(conf["dataset"], transform)
+        supplementary_dm.setup()
+        setup_timer_stop = timer()
+
+        data_loader = iter(supplementary_dm.test_dataloader())
+
+        get_timer_start = timer()
+        batch = next(data_loader)
+        get_timer_stop = timer()
+
+        print(f"Setup datamodule time: {setup_timer_stop - setup_timer_start:.4f} sec")
+        print(f"Get single batch time: {get_timer_stop - get_timer_start:.4f} sec")
+        print(f"Image batch size: {batch['image'].size()} \n")
--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .exdark import ExDark  # noqa: F401
 from .lol import LOL  # noqa: F401
 from .sice import SICE  # noqa: F401
+from .supplementary import Supplementary  # noqa: F401
