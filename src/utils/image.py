fe202fa446f708aba70e6c7af456d08759f4ccaa
SylwiaZemsta <269869@student.pwr.edu.pl>
2023-06-07 20:47:45 +0200

    Update image.py

--- src/utils/image.py
+++ src/utils/image.py
@@ -5,7 +5,11 @@ import cv2
 import numpy as np
 
 
-def read_image_cv2(path: Union[str, Path]) -> np.ndarray:
+def read_image_cv2(path: Union[str, Path], grayscale: bool = False) -> np.ndarray:
     """Read an image from a path."""
+    print(path)
     image = cv2.imread(str(path))
-    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    if grayscale:
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else:
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
