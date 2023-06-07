83f0e0853958f8a8118015b66193d4b9265766f5
SylwiaZemsta <269869@student.pwr.edu.pl>
2023-06-07 22:19:34 +0200

    Create train_RestorationNet.py

---
+++ scripts/experiments/KinD/train_RestorationNet.py
@@ -0,0 +1,64 @@
+import pytorch_lightning as pl
+import torch
+from omegaconf import OmegaConf
+from pytorch_lightning.callbacks import (
+    LearningRateMonitor,
+    ModelCheckpoint,
+    RichProgressBar,
+)
+import os
+
+from src.configs.experiments import kind_config_restore as cfg  # noqa: I900
+from src.datasets import LOLDecompositionDataModul  # noqa: I900
+from src.models import LitKinD_restore  # noqa: I900
+from src.utils import divide_chunks, save_predictions  # noqa: I900
+
+
+if __name__ == "__main__":
+
+    cfg = OmegaConf.structured(cfg)
+
+    if cfg.dataset.path_decom is not None:
+        lol_dm_decom = LOLDecompositionDataModul(config=cfg.dataset)
+    else:
+        print("No data. Training cannot be performed.")
+
+    model = LitKinD_restore(config=cfg, save_images=3)
+
+
+    callbacks = [
+        RichProgressBar(),
+        ModelCheckpoint(
+            dirpath=os.path.dirname(cfg.checkpoint_path) if cfg.checkpoint_path is not None else None,
+            monitor="val/loss_restore",
+            mode="min",
+            save_top_k=2,
+            save_last=True,
+            auto_insert_metric_name=False,
+            filename=cfg.name + "-" + cfg.model_name + "-{epoch:03d}-loss-{val/loss:.4f}",
+        ),  
+        LearningRateMonitor(logging_interval="step"),
+    ]
+
+    trainer = pl.Trainer(
+        accelerator=cfg.device,
+        devices=1,
+        callbacks=callbacks,
+        max_epochs=cfg.epochs,
+    )
+
+
+    if cfg.checkpoint_path is not None:
+        print("Loading from checkpoint: ", cfg.checkpoint_path)
+  
+        checkpoint = torch.load(cfg.checkpoint_path)
+        model.load_state_dict(checkpoint["state_dict"])
+        model.eval()
+
+    else:
+        trainer.fit(model, lol_dm_decom)
+
+    
+
+
+    
