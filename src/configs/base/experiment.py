e9c72c1052fbef1c95252504757496d504ed778e
SylwiaZemsta <269869@student.pwr.edu.pl>
2023-06-07 20:38:27 +0200

    Update experiment.py

--- src/configs/base/experiment.py
+++ src/configs/base/experiment.py
@@ -5,15 +5,18 @@ from omegaconf import MISSING
 from .data import DatasetConfig
 from .model import ModelConfig
 from .training import LossConfig, OptimizerConfig
-
+from typing import Optional
 
 @dataclass
 class ExperimentConfig:
     name: str = MISSING
-
+    model_name: Optional[str] = None
+    save_predictions: Optional[str] = False
     dataset: DatasetConfig = MISSING
     model: ModelConfig = MISSING
-    optimizer: OptimizerConfig = MISSING
+    optimizer: Optional[OptimizerConfig] = MISSING
+    optimizer_restoration: Optional[OptimizerConfig] = MISSING
+    checkpoint_path: Optional[str] = None
     loss: LossConfig = MISSING
 
     finetune: bool = False  # use when you want to have 2 stages of training
