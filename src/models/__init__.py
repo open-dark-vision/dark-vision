1ed88c6cc907fa6050c605be3a5a949257716787
SylwiaZemsta <269869@student.pwr.edu.pl>
2023-06-07 22:25:22 +0200

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@ from .IAT import IAT, LitIAT  # noqa: F401
 from .LLFlow import ConditionalEncoder, LitLLFlow  # noqa: F401
 from .SCI import SCI, LitSCI  # noqa: F401
 from .SNRT import SNRT, LitSNRT  # noqa: F401
+from .KinD import LitKinD_illumina, LitKinD_restore, LitKinD_decom, LitKinD # noqa: F401
