d254e5e4c0dbbb6557a871eb28d719dfe842ca02
SylwiaZemsta <269869@student.pwr.edu.pl>
2023-06-26 21:02:30 +0200

    Update __init__.py

--- src/configs/experiments/__init__.py
+++ src/configs/experiments/__init__.py
@@ -3,3 +3,4 @@ from .llflow import llflow_config  # noqa: F401
 from .sci import sci_config, sci_finetune_config  # noqa: F401
 from .snrt import snrt_config  # noqa: F401
 from .kind import kind_config_decom, kind_config_illumin, kind_config_restore, kind_config_finale  # noqa: F401
+from .segmentation import segment_config  # noqa: F401
