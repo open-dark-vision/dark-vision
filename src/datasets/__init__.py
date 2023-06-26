17690c87a136ec3799ad97072b96bbbdfbe180db
SylwiaZemsta <269869@student.pwr.edu.pl>
2023-06-26 21:02:35 +0200

    Update __init__.py

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -2,3 +2,4 @@ from .exdark import ExDark, ExDarkDataModule  # noqa: F401
 from .lol import LOL, LOLDataModule, LOLDecompositionDataModul  # noqa: F401
 from .sice import SICE, SICEDataModule  # noqa: F401
 from .supplementary import Supplementary, SupplementaryDataModule  # noqa: F401
+from .segmentation_dataset import SemanticDataModule  # noqa: F401
